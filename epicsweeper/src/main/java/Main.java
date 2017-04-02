28ab4db2df59536a462af5bc0db73dd9924f8840
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-04-02 19:55:40 +0300

    Instructions for testing

--- epicsweeper/src/main/java/Main.java
+++ epicsweeper/src/main/java/Main.java
@@ -1,24 +1,11 @@
-import epicminesweeper.gui.MainGui;
-
 public class Main {
 
     public static void main(String[] args) {
-
-//        Game g = new Game(10,15,10);
-//        Game g = new Game(6, 7, "" +
-//                "xoooooo" +
-//                "ooxoooo" +
-//                "xoxoooo" +
-//                "ooooxoo" +
-//                "ooooooo" +
-//                "ooooooo");
-//        Board b = g.getBoard();
-//        b.clickTile(6, 0);
-//        System.out.println(b.exportBoard(true));
-
+//        Uncomment for playable textUI
 //        TextUI ti = new TextUI();
 //        ti.init();
 
-        MainGui mg = new MainGui();
+//        Uncomment for almost-playable Graphic UI:
+//        MainGui mg = new MainGui();
     }
 }
